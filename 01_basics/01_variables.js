5ed109d1b160a35baa10ae263b111e6fc1ec62f8
AliGhuman001 <110855028+AliGhuman001@users.noreply.github.com>
2024-07-31 20:40:47 +0000

    new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1234
+let accountEmail="ali@gmail.com"
+var accountPassword="5678"
+accountCity = "Gujranwala"
+accountEmail="hubi@gmail.com"
+accountPassword="83847"
+accountCity="Jabli"
+let accountState/* if you do not assign value to a declared varibale 
+ then it will be shown undefined in the output.*/
+/*
+Due to the block scope and functional scope do not prefer to use var.
+*/
+console.table([accountEmail,accountPassword,accountCity])
+console.log(accountId) 
